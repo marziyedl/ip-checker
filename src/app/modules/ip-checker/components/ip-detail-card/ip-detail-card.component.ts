219beb58557e5a272cf1a47d33e0a15f4c607a76
marzieh.dolati@dayasoft.com <marzieh.dolati@dayasoft.com>
2023-04-23 00:26:06 +0330

    feat: 🎸 create  search box and info card
    implement designs with static data

---
+++ src/app/modules/ip-checker/components/ip-detail-card/ip-detail-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ip-detail-card',
+  templateUrl: './ip-detail-card.component.html',
+  styleUrls: ['./ip-detail-card.component.scss']
+})
+export class IpDetailCardComponent {
+
+}
--- src/app/modules/ip-checker/ip-checker.module.ts
+++ src/app/modules/ip-checker/ip-checker.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IpCheckerComponent } from './ip-checker.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { IpDetailCardComponent } from './components/ip-detail-card/ip-detail-card.component';
 
 @NgModule({
-  declarations: [IpCheckerComponent, SearchBoxComponent],
-  exports: [IpCheckerComponent, SearchBoxComponent],
+  declarations: [IpCheckerComponent, SearchBoxComponent, IpDetailCardComponent],
+  exports: [IpCheckerComponent, SearchBoxComponent,IpDetailCardComponent],
   imports: [CommonModule],
 })
 export class IpCheckerModule {}
